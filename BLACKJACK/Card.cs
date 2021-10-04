2c2b22acb6ba571425da6cd45a236093dfc903cc
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-10-04 18:02:53 -0500

    Agregar archivos de proyecto.

---
+++ BLACKJACK/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLACKJACK
+{
+    class Card
+    {
+        public string Suit { get; set; }
+        public string Symbol { get; set; }
+
+        public Card(string Suit, string Symbol)
+        {
+            this.Suit = Suit;
+            this.Symbol = Symbol;
+
+        }
+    }
+}
---
+++ BLACKJACK/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BLACKJACK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card = new Card("DAS","DSAD");
+
+            Console.WriteLine(card.Suit + card.Symbol);
+        }
+    }
+}
