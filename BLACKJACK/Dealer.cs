65d4379cbe58670f356c16a01f43e3c32078dff6
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-10-04 22:31:28 -0500

    3

--- BLACKJACK/Dealer.cs
+++ BLACKJACK/Dealer.cs
@@ -6,8 +6,13 @@ namespace BLACKJACK
 {
     class Dealer
     {
+        // Almacena todas las cartas
         public List<Card> deck;
         public List<Card> Deck { get => deck; set => deck = value; }
+
+        // Almacena las cartas del tallador
+        public List<Card> hand;
+        public List<Card> Hand { get => hand; set => hand = value; }
 
 
         public void Generate()
@@ -16,7 +21,33 @@ namespace BLACKJACK
 
             string[] suits = { "♥", "♦", "♣", "♠" };
             string[] symbols = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+            // se deben generar las 52 cartas combinando los suits con los symbols
+
              
+        }
+
+        public void Randomize()
+        {
+            // desordena y modifica la lista o arreglo
+        }
+
+
+        public void Deal()
+        {
+            // Elimina una carta
+        }
+
+
+        public void AddCard()
+        {
+            //Recibe una carta y la agrega a la mano del dealer 
+        }
+
+
+        public void Init()
+        {
+            // Invoca dos veces al metodo Deal y agrega las cartas al hand utilisando el metodo AddCard
         }
 
 
